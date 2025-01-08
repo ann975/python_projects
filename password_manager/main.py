54f7de8199cd9fea712cc6ff4ed4400d6eecde96
ann975 <179229338+ann975@users.noreply.github.com>
2025-01-08 16:38:46 -0500

    Save passwords to json file and add exception handling

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,7 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
-#import pyperclip
+import pyperclip
+import json
 
 #messagebox is a module not class so not imported with *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -21,27 +22,68 @@ def generate_password():
     #join method
     password = "".join(password_list)
 
-    password_entry.insert(0, password) #inserts password at 0 index 
-    #pyperclip.copy(password) #saves password in clipboard automatically
+    password_entry.insert(0, password) #inserts password at 0 index
+    pyperclip.copy(password) #saves password in clipboard automatically
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+#saving text to JSON: JavaScript Object Notation, way to transfer data
+#nested list and dictionaries, with key, value pair
+#write (json.dump()), read (json.load()), update (json.update())
 def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
 
+    #new dictionary
+    new_data ={
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+
     #standard dialogue
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail:{email} "
-                                                      f"\nPassword:{password} \nIs it okay to save?")
+        # with open("data.json", "w") as data_file:
+        #     # json.dump(new_data, data_file, indent=4) #write to json file, uses w mode
+        #     data = json.load(data_file) #read old data
+        #     #takes json data and converts to Python dictionary, has type dictionary, uses r mode
+        #     # print(data)
+        #     data.update(new_data) #update with new data
+        #     json.dump(data, data_file, indent=4) #write new data
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        else:
+            data.update(new_data)  # not appending but updating dictionary with new data
 
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0,END)
-                password_entry.delete(0, END)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
 
+        finally:
+            website_entry.delete(0,END)
+            password_entry.delete(0, END)
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found!")
+    else:
+        if website in data: #check if key is in dictionary
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email:{email}\nPassword:{password}" )
+        else:
+            messagebox.showinfo(title="Error", message="Website password not saved")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -51,7 +93,7 @@ window.title("Password Manager")
 window.config(padx=50, pady=50, bg="white", highlightthickness=0)
 
 canvas = Canvas(height=200, width=200, bg="white", highlightthickness=0)
-lock_image = PhotoImage(file="password_manager/logo.png")
+lock_image = PhotoImage(file="logo.png")
 canvas.create_image(100,100, image=lock_image)
 canvas.grid(row=0, column=1)
 
@@ -66,8 +108,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=38)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus() #starts cursor
 
 
@@ -85,6 +127,8 @@ generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add",width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
+search_button = Button(text="Search", width = 13, command=find_password)
+search_button.grid(row=1, column=2)
 
 
 window.mainloop()
