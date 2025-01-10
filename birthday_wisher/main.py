67fdee4b8196d9ad45539cb881bf1730c030d43f
ann975 <179229338+ann975@users.noreply.github.com>
2025-01-10 11:58:37 -0500

    Edit to name of file and README

--- birthday_wisher/main.py
+++ birthday_wisher/main.py
@@ -7,12 +7,12 @@ MY_EMAIL =  "anc52153@gmail.com"
 MY_PASSWORD = "fvqwfkrycjkywfut"
 today_tuple = (datetime.now().month, datetime.now().day)
 
-data = pandas.read_csv("birthdays.csv")
+data = pandas.read_csv("birthday_wisher/birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
-    file_path = f"./letter_templates/letter_{random.randint(1,3)}.txt"
+    file_path = f"birthday_wisher/letters_templates/letter_{random.randint(1,3)}.txt"
     with open(file_path, "r") as letter_file:
         contents = letter_file.read()
         contents = contents.replace("[NAME]",birthday_person["name"])
